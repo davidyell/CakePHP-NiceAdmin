3c57928f9258cd5d7177dcc9dca273b0897a77c3
David Yell <dyell@ukwebmedia.com>
2012-12-13 15:23:49 +0000

    Updated the Actions helper to streamline it and give it more configurability

--- View/Helper/ActionsHelper.php
+++ View/Helper/ActionsHelper.php
@@ -1,31 +1,55 @@
 <?php
 /**
- * Description of ActionsHelper
+ * Helper to replace the actions cell links in the admin baked index pages.
  *
- * @author david
+ * @author David Yell
  */
 class ActionsHelper extends AppHelper{
 
+/**
+ *  Include some core system helpers so we can output
+ * @var array
+ */
     public $helpers = array('Html', 'Form');
 
-    public function actionButtons($id){
+/**
+ * Output the links
+ * @param int $id The id of the item
+ * @param array $options array of v, e and/or d representing which button(s) to output
+ * @param string $type 'buttons' or 'icons' The type of links to generate
+ * @return type
+ */
+    public function actions($id, $options = array('v','e','d'), $type = 'buttons'){
         $html = '';
-        $html .= $this->Html->link(__('View'), array('action' => 'view', $id), array('class'=>'btn btn-small'));
-        $html .= $this->Html->link(__('Edit'), array('action' => 'edit', $id), array('class'=>'btn btn-small'));
-        $html .= $this->Form->postLink(__('Delete'), array('action' => 'delete', $id), array('class'=>'btn btn-small btn-danger'), __('Are you sure you want to delete # %s?', $id));
 
-        return $html;
-    }
+// View button
+        if(isset($options['v'])){
+            if($type == 'icons'){
+                $html .= $this->Html->image('/action_buttons/img/view.png', array('url'=>array('action' => 'view', $id),'alt'=>'View','title'=>'View'));
+            }else{
+                $html .= $this->Html->link(__('View'), array('action' => 'view', $id), array('class'=>'btn btn-small'));
+            }
+        }
 
-    public function actionIcons($id){
-        $html = '';
-        $html .= $this->Html->image('/action_buttons/img/view.png', array('url'=>array('action' => 'view', $id),'alt'=>'View','title'=>'View'));
-        $html .= $this->Html->image('/action_buttons/img/edit.png', array('url'=>array('action' => 'edit', $id),'alt'=>'Edit','title'=>'Edit'));
-        $html .= $this->Form->postLink($this->Html->image('/action_buttons/img/delete.png', array('alt'=>'Delete','title'=>'Delete')), array('action' => 'delete', $id), array('escape'=>false), __('Are you sure you want to delete # %s?', $id));
+// Edit button
+        if(isset($options['e'])){
+            if($type == 'icons'){
+                $html .= $this->Html->image('/action_buttons/img/edit.png', array('url'=>array('action' => 'edit', $id),'alt'=>'Edit','title'=>'Edit'));
+            }else{
+                $html .= $this->Html->link(__('Edit'), array('action' => 'edit', $id), array('class'=>'btn btn-small'));
+            }
+        }
+
+// Delete button
+        if(isset($options['d'])){
+            if($type == 'icons'){
+                $html .= $this->Form->postLink($this->Html->image('/action_buttons/img/delete.png', array('alt'=>'Delete','title'=>'Delete')), array('action' => 'delete', $id), array('escape'=>false), __('Are you sure you want to delete # %s?', $id));
+            }else{
+                $html .= $this->Form->postLink(__('Delete'), array('action' => 'delete', $id), array('class'=>'btn btn-small btn-danger'), __('Are you sure you want to delete # %s?', $id));
+            }
+        }
 
         return $html;
     }
 
 }
-
-?>
