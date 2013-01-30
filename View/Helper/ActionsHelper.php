6f054b9216fadc27091dafe201f4e262e34e410b
David Yell <dyell@ukwebmedia.com>
2013-01-30 16:39:26 +0000

    Resolves #1. Updated the plugin so that it can take a controller as a parameter. Also re-ordered the parameters to move controller before type, as it's more important.

--- View/Helper/ActionsHelper.php
+++ View/Helper/ActionsHelper.php
@@ -20,33 +20,41 @@ class ActionsHelper extends AppHelper {
  * @param string $type 'buttons' or 'icons' The type of links to generate
  * @return type
  */
-    public function actions($id, $options = array('v', 'e', 'd'), $type = 'buttons') {
+    public function actions($id, $options = array('v', 'e', 'd'), $controller = '', $type = 'buttons') {
+        
         $html = '';
+        
+        // Build the url
+        if(isset($controller) && !empty($controller)){
+            $url = array('controller' => $controller, 'action' => 'view', $id);
+        }else{
+            $url = array('action' => 'view', $id);
+        }
 
         // View button
         if (in_array('v', $options)) {
             if ($type == 'icons') {
-                $html .= $this->Html->image('/nice_admin/img/view.png', array('url' => array('action' => 'view', $id), 'alt' => 'View', 'title' => 'View'));
+                $html .= $this->Html->image('/nice_admin/img/view.png', array('url' => $url, 'alt' => 'View', 'title' => 'View'));
             } else {
-                $html .= $this->Html->link(__('View'), array('action' => 'view', $id), array('class' => 'btn btn-small'));
+                $html .= $this->Html->link(__('View'), $url, array('class' => 'btn btn-small'));
             }
         }
 
         // Edit button
         if (in_array('e', $options)) {
             if ($type == 'icons') {
-                $html .= $this->Html->image('/nice_admin/img/edit.png', array('url' => array('action' => 'edit', $id), 'alt' => 'Edit', 'title' => 'Edit'));
+                $html .= $this->Html->image('/nice_admin/img/edit.png', array('url' => $url, 'alt' => 'Edit', 'title' => 'Edit'));
             } else {
-                $html .= $this->Html->link(__('Edit'), array('action' => 'edit', $id), array('class' => 'btn btn-small'));
+                $html .= $this->Html->link(__('Edit'), $url, array('class' => 'btn btn-small'));
             }
         }
 
         // Delete button
         if (in_array('d', $options)) {
             if ($type == 'icons') {
-                $html .= $this->Form->postLink($this->Html->image('/nice_admin/img/delete.png', array('alt' => 'Delete', 'title' => 'Delete')), array('action' => 'delete', $id), array('escape' => false), __('Are you sure you want to delete # %s?', $id));
+                $html .= $this->Form->postLink($this->Html->image('/nice_admin/img/delete.png', array('alt' => 'Delete', 'title' => 'Delete')), $url, array('escape' => false), __('Are you sure you want to delete # %s?', $id));
             } else {
-                $html .= $this->Form->postLink(__('Delete'), array('action' => 'delete', $id), array('class' => 'btn btn-small btn-danger'), __('Are you sure you want to delete # %s?', $id));
+                $html .= $this->Form->postLink(__('Delete'), $url, array('class' => 'btn btn-small btn-danger'), __('Are you sure you want to delete # %s?', $id));
             }
         }
 
